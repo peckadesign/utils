5790f6bee359099e72a9df515f267037cbdd0ab0
Petr Klobás <petr.klobas@gmail.com>
2020-03-10 17:51:50 +0100

    peckadesign/pdproject5#2860 Aktualizace PHP pro pd/utils
    - nette/tester jako dev závislost
    - doplněny testy na české IČO
    - testy se spouští v travisu

---
+++ tests/Unit/ValidatorsTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace PdTests\Unit;
+
+require __DIR__ . '/../bootstrap.php';
+
+/**
+ * @testCase
+ */
+final class ValidatorsTest extends \Tester\TestCase
+{
+
+	/**
+	 * @dataProvider provideValidIdentifiers
+	 */
+	public function testCzechCompanyIdentifierPassing(string $identifier): void
+	{
+		\Tester\Assert::true(\Pd\Utils\Validators::isCzechCompanyIdentifier($identifier));
+	}
+
+
+	/**
+	 * @dataProvider provideInvalidIdentifiers
+	 */
+	public function testCzechCompanyIdentifierFailing(string $identifier): void
+	{
+		\Tester\Assert::false(\Pd\Utils\Validators::isCzechCompanyIdentifier($identifier));
+	}
+
+
+	public function provideValidIdentifiers(): array
+	{
+		return [
+			['26266644'],
+			['02652269'],
+			['26168685'],
+			['88114163'],
+		];
+	}
+
+
+	public function provideInvalidIdentifiers(): array
+	{
+		return [
+			['CZ26266644'],
+			['blbost'],
+			['12345678'],
+			['\n  ']
+		];
+	}
+
+
+}
+
+(new \PdTests\Unit\ValidatorsTest())->run();
